415be7c3016f8def7ccb9b3cded802e1fb829826
Aginscale <ashleynguyen2935@gmail.com>
2025-04-02 14:36:50 -0500

    Transition Function

--- LEDs.ino
+++ LEDs.ino
@@ -12,7 +12,7 @@
 
 #define LEDPin 18 // pin 18 controls strip
 #define NumLEDs 195 // about 195 LEDS per 4ft (for 1 panel)
-#define Brightness 50 // controls LED brightness (0-255) [significant heat produced over 100]
+#define Brightness 30 // controls LED brightness (0-255) [significant heat produced over 100]
 
 Adafruit_NeoPixel strip(NumLEDs, LEDPin, NEO_GRB + NEO_KHZ800); // defines strip object (800KHZ, GRB required for strip model)
 
@@ -40,14 +40,22 @@ void loop() {
 // ...
 
 // example code turns all LEDS red, green, then blue for 1 second each
-    SetAllLEDs(strip.Color(255, 0, 0)); // All LEDs Red
+    
+    /*SetAllLEDs(strip.Color(255, 0, 0)); // All LEDs Red
     delay(1000);
 
     SetAllLEDs(strip.Color(0, 255, 0)); // All LEDs Green
     delay(1000);
 
     SetAllLEDs(strip.Color(0, 0, 255)); // All LEDs Blue
+    delay(1000);*/
+    
+    //To test out, gradients from blue to orange porportionally in 2s
+    TransitionLEDs(strip.Color(255,0,0), strip.Color(0,0,255));
     delay(1000);
+    TransitionLEDs(strip.Color(0,0,255), strip.Color(255,0,0));
+    delay(1000);
+    
 
 }
 
@@ -61,3 +69,35 @@ void SetAllLEDs(uint32_t color) {
 
 // any other LED animation functions can be included here
 // ...
+
+//WIP CODE to test out
+void TransitionLEDs(uint32_t color1, uint32_t color2){
+  int red1 = (color1 >> 16) & 0xff;
+  int green1 = (color1 >> 8) & 0xff;
+  int blue1 = (color1) & 0xff;
+
+  int red2 = (color2 >> 16) & 0xff;
+  int green2 = (color2 >> 8) & 0xff;
+  int blue2 = (color2) & 0xff;
+
+  int redchange = (red2 - red1)/200;
+  int greenchange = (green2 - green1)/200;
+  int bluechange = (blue2 - blue1)/200;
+
+  int newred;
+  int newgreen;
+  int newblue;
+
+  for (int i = 0; i<199;i++){
+    newred = int(red1 + (redchange * i));
+    newgreen = int(green1 + (greenchange * i));
+    newblue = int(blue1 + (bluechange * i));
+
+    SetAllLEDs(strip.Color(newred, newgreen, newblue));
+    delay(10);
+  }
+
+  SetAllLEDs(strip.Color(red2, green2, blue2));
+}
+
+
